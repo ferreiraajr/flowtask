a149e3d11638461e4a92f2d5f735b99bfa5dd835
Ferreira Junior <ferreirajr026@gmail.com>
2025-08-28 08:39:36 -0300

    feat: adiciona relacionamento de workspaces ao modelo User e cria migração para tabela workspace_user

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WorkspaceController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -10,6 +11,9 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Rotas para Workspaces
+Route::resource('workspaces', WorkspaceController::class);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
